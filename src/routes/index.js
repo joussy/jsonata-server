dfcd5424f7c0876595a23f16b9b63349fa1af21e
joussy <joussyjousse@gmail.com>
2024-06-04 20:00:54 +0000

    jpp

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const jsonata = require('jsonata');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = router; 
