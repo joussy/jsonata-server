40c96991a9b0888e50262f48484bc97f1463bb8b
joussy <joussyjousse@gmail.com>
2024-06-05 21:31:35 +0000

    docker and server side node

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,38 @@ var express = require('express');
 var router = express.Router();
 const jsonata = require('jsonata');
 
-router.get('/', function(req, res, next) {
-  var res = jsonata().evaluate(req, {});
+router.get('/', function (req, res, next) {
+  // var res = jsonata().evaluate(req, {});
   console.log(res);
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/jsonata', function (req, res, next) {
+  try {
+    jsonInput = JSON.parse(req.body.input);
+  }
+  catch (error) {
+    res.send(`Input error:\n${error.message}`);
+    return;
+  }
+  if (!jsonInput) {
+    res.send("");
+    return;
+  }
+  try {
+    const compiledExpression = jsonata(req.body.expression);
+    compiledExpression.evaluate(jsonInput, {}, (error, success) => {
+      if (error) {
+        res.send(`evaluation error:\n${error}`);
+      } else {
+        res.send(success ?? "");
+      }
+    });
+  }
+  catch (error) {
+    const errorText = JSON.stringify(error, null, 2);
+    res.send(`Template error:\n${errorText}`);
+  }
 });
 
 module.exports = router;
