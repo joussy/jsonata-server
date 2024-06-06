cc7132daf1d06d8b6af2ee9eba45f3ee6bfdc9fd
joussy <joussyjousse@gmail.com>
2024-06-06 21:00:52 +0000

    csv output mode

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const jsonata = require('jsonata');
+const { stringify } = require('csv-stringify');
+const { promisify } = require('util');
+
+const stringifyAsync = promisify(stringify);
 
 router.get('/', function (req, res, next) {
   // var res = jsonata().evaluate(req, {});
@@ -8,31 +12,50 @@ router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.post('/jsonata', function (req, res, next) {
+
+router.post('/jsonata', async (req, res, next) => {
+  let jsonInput;
+
+  // Parse JSON input
   try {
     jsonInput = JSON.parse(req.body.input);
+  } catch (error) {
+    return res.status(400).send({ error: `Input error: ${error.message}` });
   }
-  catch (error) {
-    res.send(`Input error:\n${error.message}`);
-    return;
-  }
+
+  // Check for empty JSON input
   if (!jsonInput) {
-    res.send("");
-    return;
+    return res.status(400).send({ error: "Empty JSON input" });
   }
+
   try {
+    // Compile JSONata expression
     const compiledExpression = jsonata(req.body.expression);
-    compiledExpression.evaluate(jsonInput, {}, (error, success) => {
-      if (error) {
-        res.send(`evaluation error:\n${error}`);
-      } else {
-        res.send(success ?? "");
+
+    // Evaluate the expression
+    const jsonResult = await compiledExpression.evaluate(jsonInput);
+
+    if (req.body.csvOutput) {
+      // Check if the result is an array
+      if (!Array.isArray(jsonResult)) {
+        return res.status(400).send({ error: "CSV output error: Result is not an array" });
       }
-    });
-  }
-  catch (error) {
-    const errorText = JSON.stringify(error, null, 2);
-    res.send(`Template error:\n${errorText}`);
+
+      // Convert result to CSV
+      try {
+        const csvOutput = await stringifyAsync(jsonResult, { header: true });
+        return res.send(csvOutput);
+      } catch (err) {
+        return res.status(400).send({ error: `CSV output error: ${err.message}` });
+      }
+    }
+    else
+    {
+      return res.send(jsonResult);
+    }
+
+  } catch (error) {
+    return res.status(400).send({ error: `Template error: ${JSON.stringify(error, null, 2)}` });
   }
 });
 
