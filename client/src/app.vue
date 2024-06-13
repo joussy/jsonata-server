07d4245b65a18470dda2a11596f2a6471bcb0c6a
joussy <joussyjousse@gmail.com>
2024-06-13 18:51:46 +0000

    server client split

---
+++ client/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+Hello World !
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+
+  }
+});
+</script>
+
+<style>
+/* Add your component-specific styles here */
+</style>
---
+++ client/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./app.vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+createApp(App).mount("#app"); 
